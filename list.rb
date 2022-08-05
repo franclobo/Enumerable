0754d52c9407c4c167d8de183929626f8660e838
franclobo <fjbl2788@gmail.com>
2022-08-05 12:26:16 -0500

    Fix linter errors

--- list.rb
+++ list.rb
@@ -5,9 +5,9 @@ class MyList
   attr_reader :list
 
   def initialize(*args)
-        @list = []
-        args.each { |arg| @list << arg }
-end
+    @list = []
+    args.each { |arg| @list << arg }
+  end
 
   def each(&block)
     @list.each(&block)
