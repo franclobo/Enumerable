a1612608cdb46c9f4b4c54d3ed251191262ccc0a
franclobo <fjbl2788@gmail.com>
2022-08-05 11:57:50 -0500

    Install linters.

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,12 +1,26 @@
 module MyEnumerable
 
   def all_method
+    list.each do |e|
+      return false unless yield(e)
+    end
+    true
   end
 
   def any_method
+    list.each do |e|
+      return true if yield(e)
+    end
+    false 
   end
 
   def filter_method
+    result = []
+    list.each do |e|
+      return list if yield(e)
+    end
+    result
   end
-
 end
+
+
--- MyList.rb
+++ MyList.rb
@@ -1,7 +1,28 @@
-require "MyEnumerable.rb"
+require_relative "MyEnumerable.rb"
 
 class MyList
-  include 
-  @list = ''
-  
+  include MyEnumerable
+  attr_accessor :list
+
+  def initialize(list)
+    @list = list
+  end
+
+  def each(&block)
+    list.each(&block)
+  end
 end
+
+# Create our list
+list = MyList.new([1, 2, 3, 4])
+
+# Test #all?
+p(list.all_method {|e| e < 5})
+p(list.all_method {|e| e > 5})
+
+# Test #any?
+p(list.any_method {|e| e == 2})
+p(list.any_method {|e| e == 5})
+
+# Test #filter
+p(list.filter_method {|e| e.even?})
